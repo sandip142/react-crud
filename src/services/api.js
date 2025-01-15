39e6c53d0fd904c748c07aeec871c30d2c994ccd
sandip kankhare <sandipkankhare940@gmail.com>
2025-01-15 20:47:46 +0530

    change the Api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/users",
+  baseURL: "https://backend-six-taupe.vercel.app/api/users",
 });
 
 export default api;
